52be421c3697262948d632ebca4541e555e61d67
yemane <yemanemeasho2021@gmail.com>
2026-07-22 01:44:45 +0200

    Fix static prerender error with Cloudflare context2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { getPdfTree, findFirstPdf } from "@/lib/pdfs";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const tree = await getPdfTree();
 
